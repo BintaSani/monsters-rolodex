0f52e524052e06be525d0508aed95c98f7169174
Binta Sani <bintasani992@gmail.com>
2023-06-10 01:42:10 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';  
+
+//import { CardList } from './components/card-list/card-list.component';
+
+//import { SearchBox } from './components/search-box/search-box.component';
+
+import logo from './logo.svg';
+
+import './App.css';
+
+/*class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }))
+  }
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value})
+  }
+
+  render(){
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={this.handleChange}
+        />
+       <CardList monsters={filteredMonsters} />
+       
+      </div>
+    );
+  }
+}
+
+/*function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello. my name is Binta.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+*/
+class App extends React.Component{
+    state ={
+      meaningOfLife: 47
+    }
+
+  handleClick = () => {
+    this.setState((prevState, prevProps) => {
+    return {meaningOfLife: prevState.meaningOfLife + 1}
+    },
+      () => console.log(this.state.meaningOfLife))
+  }
+
+  render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {this.state.meaningOfLife}
+        </p>
+        <button
+           onClick={this.handleClick}
+        >
+          Update State
+        </button>
+      </header>
+    </div>
+  );
+  }
+}
+
+export default App;
+
+
